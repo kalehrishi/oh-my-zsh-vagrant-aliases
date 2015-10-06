cd6d68c389fce67612d44b67cc2c992344413184
Hrishikesh Kale <kalehrishi@gmail.com>
2015-10-06 09:50:59 +0530

    added initial set of vagarnt aliases

---
+++ vagrantaliases.plugin.zsh
@@ -0,0 +1,7 @@
+alias vu='vagrant up'
+alias vh='vagrant halt'
+alias vd='vagrant destroy'
+alias vpr='vagrant global-status --prune'
+alias vbl='vagrant box list'
+alias vidb75='vagrant init debian75-x64-11'
+alias vidb8='vagrant init debian81x64-00'
